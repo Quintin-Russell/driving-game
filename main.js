410b00a31afa43d4bcc1dab2b8ec041b75aba60d
Quintin Russell <quinn.j.russell@gmail.com>
2021-06-29 20:34:51 -0700

    revised turning functions to update class of html car object

--- main.js
+++ main.js
@@ -1,36 +1,48 @@
 // select pg-container and img
-/* const $pg = document.querySelector('.pg-container');
-const $car = document.querySelector('.car-img');
+const $car = document.querySelector('#car');
 let rotation = 0;
+const offset = 90;
 
 // make turning functions: L(-90deg), R (+90deg)
-const rotate = function (rotation, offset) {
-  let newRotation = (rotation + offset);
-  newRotation = '"rotate(' + newRotation + 'deg)"';
-  $car.setAttribute('transform', newRotation);
-};
 const turnCW = function () {
-  const offset = 90;
-  if (rotation === 360) {
+  if (rotation === 0) {
+    $car.setAttribute('class', 'rotate90');
+    rotation += offset;
+  } else if (rotation === 90) {
+    $car.setAttribute('class', 'rotate180');
+    rotation += offset;
+  } else if (rotation === 180) {
+    $car.setAttribute('class', 'rotate270');
+    rotation += offset;
+  } else if (rotation === 270) {
+    $car.setAttribute('class', 'rotate0');
     rotation = 0;
-    rotate(rotation, offset);
-  } else {
-    rotate(rotation, offset);
   }
 };
 
 const turnCCW = function () {
-  const offset = -90;
   if (rotation === 0) {
-    rotation = 360;
-    rotate(rotation, offset);
-  } else {
-    rotate(rotation, offset);
+    $car.setAttribute('class', 'rotate270');
+    rotation = 270;
+  } else if (rotation === 90) {
+    $car.setAttribute('class', 'rotate0');
+    rotation -= offset;
+  } else if (rotation === 180) {
+    $car.setAttribute('class', 'rotate90');
+    rotation -= offset;
+  } else if (rotation === 270) {
+    $car.setAttribute('class', 'rotate180');
+    rotation -= offset;
   }
 };
 
 // add event listener to pg: target img, event: keydown(down-arrow), function
-// $pg.addEventListener("keydown", function(e){
-//  if (event.target === )
-// });
-*/
+document.addEventListener('keydown', function (e) {
+  if ((event.code === 'ArrowRight') || (event.code === 'ArrowLeft')) {
+    if (event.code === 'ArrowLeft') {
+      turnCCW();
+    } else {
+      turnCW();
+    }
+  }
+});
