91baa37418ffaeafd4037f6d4a790d20edcb6e7c
Quintin Russell <quinn.j.russell@gmail.com>
2021-07-01 21:58:54 -0700

    added directional functions and modified turning functions

--- main.js
+++ main.js
@@ -11,61 +11,28 @@ let int;
 const $car = document.querySelector('#car');
 const offset = 90;
 
-// make turning functions: L(-90deg), R (+90deg)
-const turnCW = function () {
-  if (carro.rotation === 0) {
-    $car.setAttribute('class', 'rotate90');
-    carro.rotation += offset;
-  } else if (carro.rotation === 90) {
-    $car.setAttribute('class', 'rotate180');
-    carro.rotation += offset;
-  } else if (carro.rotation === 180) {
-    $car.setAttribute('class', 'rotate270');
-    carro.rotation += offset;
-  } else if (carro.rotation === 270) {
-    $car.setAttribute('class', 'rotate0');
-    carro.rotation = 0;
-  }
-};
-
-const turnCCW = function () {
-  if (carro.rotation === 0) {
-    $car.setAttribute('class', 'rotate270');
-    carro.rotation = 270;
-  } else if (carro.rotation === 90) {
-    $car.setAttribute('class', 'rotate0');
-    carro.rotation -= offset;
-  } else if (carro.rotation === 180) {
-    $car.setAttribute('class', 'rotate90');
-    carro.rotation -= offset;
-  } else if (carro.rotation === 270) {
-    $car.setAttribute('class', 'rotate180');
-    carro.rotation -= offset;
-  }
-};
-/*
 // functions that incriment carro.x & .y and set intervals
 function plusX() {
   carro.x += 10;
-  setInterval(function () {
+  int = setInterval(function () {
     $car.style.left = (carro.x + 'px');
   }, 16);
 }
 function minusX() {
   carro.x -= 10;
-  setInterval(function () {
+  int = setInterval(function () {
     $car.style.left = (carro.x + 'px');
   }, 16);
 }
 function plusY() {
   carro.y += 10;
-  setInterval(function () {
+  int = setInterval(function () {
     $car.style.up = (carro.y + 'px');
   }, 16);
 }
 function minusY() {
   carro.y -= 10;
-  setInterval(function () {
+  int = setInterval(function () {
     $car.style.up = (carro.y + 'px');
   }, 16);
 }
@@ -74,13 +41,64 @@ function minusY() {
 function clear() {
   clearInterval(int);
 }
-*/
+
+// make turning functions: L(-90deg), R (+90deg)
+const turnCW = function () {
+  if (carro.rotation === 0) {
+    $car.setAttribute('class', 'rotate90');
+    carro.rotation += offset;
+    clear();
+    minusY();
+  } else if (carro.rotation === 90) {
+    $car.setAttribute('class', 'rotate180');
+    carro.rotation += offset;
+    clear();
+    minusX();
+  } else if (carro.rotation === 180) {
+    $car.setAttribute('class', 'rotate270');
+    carro.rotation += offset;
+    clear();
+    plusY();
+  } else if (carro.rotation === 270) {
+    $car.setAttribute('class', 'rotate0');
+    carro.rotation = 0;
+    clear();
+    plusX();
+  }
+};
+
+const turnCCW = function () {
+  if (carro.rotation === 0) {
+    $car.setAttribute('class', 'rotate270');
+    carro.rotation = 270;
+    clear();
+    plusY();
+  } else if (carro.rotation === 90) {
+    $car.setAttribute('class', 'rotate0');
+    carro.rotation -= offset;
+    clear();
+    plusX();
+  } else if (carro.rotation === 180) {
+    $car.setAttribute('class', 'rotate90');
+    carro.rotation -= offset;
+    clear();
+    minusY();
+  } else if (carro.rotation === 270) {
+    $car.setAttribute('class', 'rotate180');
+    carro.rotation -= offset;
+    clear();
+    minusX();
+  }
+};
+
 // add event listener to pg: target img, event: keydown(down-arrow), function
 document.addEventListener('keydown', function (e) {
   if ((event.code === 'ArrowRight') || (event.code === 'ArrowLeft')) {
     if (event.code === 'ArrowLeft') {
+      clear();
       turnCCW();
     } else {
+      clear();
       turnCW();
     }
   }
@@ -90,14 +108,10 @@ document.addEventListener('keydown', function (e) {
 document.addEventListener('keydown', function (e) {
   if (event.code === 'Space') {
     if (carro.moving === 0) {
-      int = setInterval(function () {
-        carro.moving = 1;
-        carro.x += 10;
-        $car.style.left = (carro.x + 'px');
-      }, 16);
+      plusX();
     } else {
       carro.moving = 0;
-      clearInterval(int);
+      clear();
     }
   }
 });
