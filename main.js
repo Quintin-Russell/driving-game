44d9c72df7ca4c1a904acb54475b3566466a5cf5
Quintin Russell <quinn.j.russell@gmail.com>
2021-07-01 21:03:26 -0700

    edited listener on spacebar to allow stopping

--- main.js
+++ main.js
@@ -1,40 +1,46 @@
+// set carro object
+const carro = {
+  rotation: null,
+  x: null,
+  y: null,
+  moving: 0
+};
+let int;
+
 // select pg-container and img
 const $car = document.querySelector('#car');
-let rotation = 0;
-let x = 0;
-// const y = 0;
 const offset = 90;
 
 // make turning functions: L(-90deg), R (+90deg)
 const turnCW = function () {
-  if (rotation === 0) {
+  if (carro.rotation === 0) {
     $car.setAttribute('class', 'rotate90');
-    rotation += offset;
-  } else if (rotation === 90) {
+    carro.rotation += offset;
+  } else if (carro.rotation === 90) {
     $car.setAttribute('class', 'rotate180');
-    rotation += offset;
-  } else if (rotation === 180) {
+    carro.rotation += offset;
+  } else if (carro.rotation === 180) {
     $car.setAttribute('class', 'rotate270');
-    rotation += offset;
-  } else if (rotation === 270) {
+    carro.rotation += offset;
+  } else if (carro.rotation === 270) {
     $car.setAttribute('class', 'rotate0');
-    rotation = 0;
+    carro.rotation = 0;
   }
 };
 
 const turnCCW = function () {
-  if (rotation === 0) {
+  if (carro.rotation === 0) {
     $car.setAttribute('class', 'rotate270');
-    rotation = 270;
-  } else if (rotation === 90) {
+    carro.rotation = 270;
+  } else if (carro.rotation === 90) {
     $car.setAttribute('class', 'rotate0');
-    rotation -= offset;
-  } else if (rotation === 180) {
+    carro.rotation -= offset;
+  } else if (carro.rotation === 180) {
     $car.setAttribute('class', 'rotate90');
-    rotation -= offset;
-  } else if (rotation === 270) {
+    carro.rotation -= offset;
+  } else if (carro.rotation === 270) {
     $car.setAttribute('class', 'rotate180');
-    rotation -= offset;
+    carro.rotation -= offset;
   }
 };
 
@@ -52,9 +58,15 @@ document.addEventListener('keydown', function (e) {
 // add eventlistener to listen for space bar and start interval
 document.addEventListener('keydown', function (e) {
   if (event.code === 'Space') {
-    setInterval(function () {
-      x += 10;
-      $car.style.left = (x + 'px');
-    }, 16);
+    if (carro.moving === 0) {
+      int = setInterval(function () {
+        carro.moving = 1;
+        carro.x += 10;
+        $car.style.left = (carro.x + 'px');
+      }, 16);
+    } else {
+      carro.moving = 0;
+      clearInterval(int);
+    }
   }
 });
